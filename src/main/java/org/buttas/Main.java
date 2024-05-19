8079cf72aa746ad84abc346469f73f9590d2c234
Sushant Butta <sushant.butta@gmail.com>
2024-05-19 15:02:01 +0530

    initial commit

---
+++ src/main/java/org/buttas/Main.java
@@ -0,0 +1,15 @@
+package org.buttas;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Sushant";
+
+        System.out.println(name.length());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.concat(" Butta"));
+        System.out.println(name.getClass());
+        System.out.println(name.charAt(5));
+
+    }
+}
